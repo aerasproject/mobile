ae19dceefbba899fb8a81b27a6b01494660f1f7d
rbpolim <rbpolim@gmail.com>
2023-08-14 20:00:09 -0300

    feat: new theme file design system

---
+++ src/theme/index.ts
@@ -0,0 +1,23 @@
+export default {
+  COLORS: {
+    LINEAR_GRADIENT: ['#22BDF4', '#0067E9'],
+    WHITE: '#FFFFFF',
+    BRAND: '#0051B6',
+    GREEN_LIGHT: '#00ECA0',
+    GRAY: '#50555C',
+  },
+  // FONT_FAMILY: {
+  //   REGULAR: 'Roboto_400Regular',
+  //   MEDIUM: 'Roboto_500Medium',
+  //   BOLD: 'Roboto_700Bold'
+  // },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 20,
+    XXL: 24,
+    XXXL: 32,
+  }
+};
