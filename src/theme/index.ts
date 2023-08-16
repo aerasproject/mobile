4da88d79860476c9120a4ffdda58a4e6142d0a34
rbpolim <rbpolim@gmail.com>
2023-08-16 12:23:30 -0300

    styles: updated theme colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,16 +1,19 @@
 export default {
   COLORS: {
     LINEAR_GRADIENT: ['#22BDF4', '#0067E9'],
+
     WHITE: '#FFFFFF',
+
     BRAND: '#0051B6',
+
     GREEN_LIGHT: '#00ECA0',
-    GRAY: '#50555C',
+
+    GRAY_100: '#e1e1e1',
+    GRAY_300: '#96999d',
+    GRAY_500: '#50555C',
+
+    ERROR: '#F44336',
   },
-  // FONT_FAMILY: {
-  //   REGULAR: 'Roboto_400Regular',
-  //   MEDIUM: 'Roboto_500Medium',
-  //   BOLD: 'Roboto_700Bold'
-  // },
   FONT_SIZE: {
     XS: 12,
     SM: 14,
@@ -19,5 +22,10 @@ export default {
     XL: 20,
     XXL: 24,
     XXXL: 32,
-  }
-};
+  },
+  // FONT_FAMILY: {
+  //   REGULAR: 'Roboto_400Regular',
+  //   MEDIUM: 'Roboto_500Medium',
+  //   BOLD: 'Roboto_700Bold'
+  // },
+}
