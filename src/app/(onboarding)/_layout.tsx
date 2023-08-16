a450b163ac9a98cdcae2e123194d0bbc1a5a3e60
rbpolim <rbpolim@gmail.com>
2023-08-16 12:24:47 -0300

    chore: improved Layout page

--- src/app/(onboarding)/_layout.tsx
+++ src/app/(onboarding)/_layout.tsx
@@ -1,5 +1,34 @@
-import { Stack } from 'expo-router'
+import { Stack, useNavigation } from 'expo-router'
+
+import { TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons'
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />
+  const navigation = useNavigation()
+
+  return (
+    <Stack
+      screenOptions={{
+        animation: 'fade',
+        headerStyle: {
+          backgroundColor: '#0167E9',
+        },
+      }}
+    >
+      <Stack.Screen name="welcome/index" />
+      <Stack.Screen name="choice/index" />
+      <Stack.Screen
+        name="sign-up/index"
+        options={{
+          headerTitle: '',
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <AntDesign name="leftcircleo" size={40} color="white" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
+  )
 }
