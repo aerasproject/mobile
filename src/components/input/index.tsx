3d7e4c9431f71ea8099e91cbb127632e47f0f22a
rbpolim <rbpolim@gmail.com>
2023-08-16 12:24:34 -0300

    feat: new input component w/ styles

---
+++ src/components/input/index.tsx
@@ -0,0 +1,18 @@
+import { TextInputProps } from 'react-native'
+
+import * as S from './styles'
+
+type InputProps = TextInputProps & {
+  label: string
+  errorMessage?: string
+}
+
+export function Input({ label, errorMessage, ...rest }: InputProps) {
+  return (
+    <S.Container>
+      <S.Label hasError={errorMessage}>{label}</S.Label>
+      <S.Input hasError={errorMessage} {...rest} />
+      {!!errorMessage && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
+    </S.Container>
+  )
+}
---
+++ src/components/input/styles.ts
@@ -0,0 +1,37 @@
+import { TextInput } from 'react-native'
+import styled from 'styled-components/native'
+
+type Props = {
+  hasError?: string
+}
+
+export const Container = styled.View`
+  flex: 1;
+  gap: 8px;
+  background-color: transparent;
+  border-radius: 8px;
+`
+
+export const Label = styled.Text<Props>`
+  padding-left: 8px;
+  color: ${({ theme, hasError }) =>
+    hasError ? theme.COLORS.ERROR : theme.COLORS.GRAY_300};
+`
+
+export const Input = styled(TextInput).attrs<Props>({
+  placeholderTextColor: '#96999d',
+}) <Props>`
+  border-radius: 8px;
+  height: 56px;
+  padding: 0 16px;
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+  border: ${({ theme, hasError }) =>
+    hasError
+      ? `1px solid ${theme.COLORS.ERROR}`
+      : `1px solid ${theme.COLORS.GRAY_100}`};
+`
+
+export const ErrorMessage = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.XS}px;
+  color: ${({ theme }) => theme.COLORS.ERROR};
+`
