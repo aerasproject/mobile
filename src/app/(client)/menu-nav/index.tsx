4ad295e31e11aa3767919e5d30d8de61aad16575
rbpolim <rbpolim@gmail.com>
2023-08-17 19:01:17 -0300

    chore: improved menu-nav component

--- src/app/(client)/menu-nav/index.tsx
+++ src/app/(client)/menu-nav/index.tsx
@@ -1,9 +1,57 @@
-import { Text, View } from 'react-native'
+import { Href, Link } from 'expo-router'
+import { Feather } from '@expo/vector-icons'
+
+import { Button } from '@/components/button'
+
+import * as S from './styles'
+
+type Icon = keyof typeof Feather.glyphMap
 
 export default function MenuNav() {
+  const routes = [
+    {
+      icon: 'home',
+      label: 'Início',
+      href: `/(client)/dashboard/`,
+    },
+    {
+      icon: 'settings',
+      label: 'Equipamentos',
+      href: `/(client)/dashboard/`,
+    },
+    {
+      icon: 'map-pin',
+      label: 'Endereços',
+      href: `/(client)/dashboard/`,
+    },
+    {
+      icon: 'shopping-bag',
+      label: 'Aeras Assist',
+      href: `/(client)/dashboard/`,
+    },
+    {
+      icon: 'message-square',
+      label: 'FAQ',
+      href: `/(client)/dashboard/`,
+    },
+  ]
+
   return (
-    <View>
-      <Text>MenuNav</Text>
-    </View>
+    <S.Container>
+      <S.Header>
+        <S.Username>Rodrigo Polim</S.Username>
+        <Button variants="white-outline" title="Ver e editar perfil" />
+      </S.Header>
+      <S.Content>
+        {routes.map((route) => (
+          <Link asChild href={route.href as Href<string>} key={route.label}>
+            <S.ItemNav>
+              <Feather name={route.icon as Icon} size={32} color="#0051B6" />
+              <S.ItemTitle>{route.label}</S.ItemTitle>
+            </S.ItemNav>
+          </Link>
+        ))}
+      </S.Content>
+    </S.Container>
   )
 }
--- src/app/(client)/menu-nav/styles.ts
+++ src/app/(client)/menu-nav/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components/native'
+import { Link } from 'expo-router'
 
-export const Container = styled.View``
+export const Container = styled.View`
+  flex: 1;
+`
+
+export const Header = styled.View`
+  padding: 26px;
+  gap: 16px;
+  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.BRAND};
+`
+
+export const Username = styled.Text`
+  text-align: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXXL};
+  font-weight: bold;
+  color: ${({ theme }) => theme.COLORS.WHITE};
+`
+
+export const Content = styled.View`
+  padding: 26px;
+  gap: 40px;
+`
+
+export const ItemNav = styled.Pressable`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+`
+
+export const ItemTitle = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD};
+`
+
+export const ItemSubtitle = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM};
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+`
