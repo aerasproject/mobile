3b788a0b4d0a31328335f334608efc163b196544
rbpolim <rbpolim@gmail.com>
2023-08-18 20:10:28 -0300

    feat: new file storage-user w/ methods

---
+++ src/storage/storage-user.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { UserDTO } from '@/dtos/user-dto'
+
+import { USER_STORAGE } from './storage-config'
+
+export async function storageUserSave(user: UserDTO) {
+  await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user))
+}
+
+export async function storageUserLoad(): Promise<UserDTO> {
+  const storage = await AsyncStorage.getItem(USER_STORAGE)
+
+  console.log('storage', storage)
+
+  const user: UserDTO = storage ? JSON.parse(storage) : {}
+
+  return user
+}
