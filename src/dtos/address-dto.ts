ceebd35b51c1fa5d8ef42a088fbb5b1a4f6d1b7e
rbpolim <rbpolim@gmail.com>
2023-08-19 19:46:37 -0300

    feat: address-dto file

---
+++ src/dtos/address-dto.ts
@@ -0,0 +1,13 @@
+export type AddressDTO = {
+  id: number
+  name: string
+  street: string
+  number: string
+  complement: string
+  neighborhood: string
+  city: string
+  state: string
+  postalCode: string
+  isFavorite: boolean
+  userId: number
+}
