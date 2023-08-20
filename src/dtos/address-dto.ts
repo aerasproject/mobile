8bfa92cd69698392cb663b17a7edfcdb5b0a1414
rbpolim <rbpolim@gmail.com>
2023-08-20 18:56:00 -0300

    chore: updated address-dto

--- src/dtos/address-dto.ts
+++ src/dtos/address-dto.ts
@@ -1,13 +1,11 @@
 export type AddressDTO = {
-  id: number
   name: string
   street: string
   number: string
-  complement: string
+  complement?: string
   neighborhood: string
   city: string
   state: string
-  postalCode: string
-  isFavorite: boolean
-  userId: number
+  postalCode?: string
+  isFavorite?: boolean
 }
