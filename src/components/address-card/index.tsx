794ffd53affb89a71cea2d423cc39faaf3759564
rbpolim <rbpolim@gmail.com>
2023-08-20 18:56:50 -0300

    chore: improved address-card component

--- src/components/address-card/index.tsx
+++ src/components/address-card/index.tsx
@@ -1,13 +1,20 @@
 import { AddressDTO } from '@/dtos/address-dto'
 
+import { Button } from '../button'
+
 import * as S from './styles'
 
 type AddressCardProps = {
   address: AddressDTO
   isMain?: boolean
+  openModal: () => void
 }
 
-export function AddressCard({ address, isMain = false }: AddressCardProps) {
+export function AddressCard({
+  address,
+  isMain = false,
+  openModal,
+}: AddressCardProps) {
   const { name, number } = address
 
   const fullAddress = name ? `${name}, ${number}` : 'Endereço sem nome'
@@ -15,8 +22,11 @@ export function AddressCard({ address, isMain = false }: AddressCardProps) {
   return (
     <S.Container isMain={isMain}>
       {!!isMain && <S.Badge>Sendo visualizado</S.Badge>}
+
       <S.Name>{address.name}</S.Name>
       <S.Street>{fullAddress}</S.Street>
+
+      <Button title="Ver detalhes" onPress={openModal} />
     </S.Container>
   )
 }
