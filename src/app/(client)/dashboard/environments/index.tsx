ec66d77883e5bbaf70b9c8ab5dd26e3f81fbe97f
rbpolim <rbpolim@gmail.com>
2023-08-21 11:48:58 -0300

    feat: new environments screen w/ styles

---
+++ src/app/(client)/dashboard/environments/index.tsx
@@ -0,0 +1,70 @@
+import { Alert, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { useLocalSearchParams } from 'expo-router'
+import { FlatList } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
+
+import { api } from '@/lib/axios'
+import { AppError } from '@/utils/app-error'
+import { EnvironmentDTO } from '@/dtos/enviroment-dto'
+
+import { Button } from '@/components/button'
+
+import * as S from './styles'
+
+export default function Environments() {
+  const { addressId, addressName } = useLocalSearchParams()
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [environments, setEnvironments] = useState<EnvironmentDTO[]>([])
+
+  async function fetchEnvironments() {
+    try {
+      setIsLoading(true)
+
+      const response = await api.get(`/environment/${addressId}`)
+
+      setEnvironments(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      if (isAppError) {
+        Alert.alert(error.message)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchEnvironments()
+  }, [addressId])
+
+  if (isLoading) {
+    return <Text>Loading...</Text>
+  }
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.Title>Ambientes Cadastrados</S.Title>
+        <S.Subtitle>Endereço: {addressName}</S.Subtitle>
+      </S.Header>
+
+      <FlatList
+        data={environments}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <S.EnvironmentCard>
+            <S.EnvironmentCardTitle>{item.name}</S.EnvironmentCardTitle>
+            <Feather name="trash-2" size={20} color="red" />
+          </S.EnvironmentCard>
+        )}
+      />
+
+      <Button
+        title="Cadastrar novo ambiente"
+        onPress={() => console.log('TODO: Cadastrar novo ambiente')}
+      />
+    </S.Container>
+  )
+}
---
+++ src/app/(client)/dashboard/environments/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  gap: 32px;
+  padding: 32px 26px;
+`
+
+export const Header = styled.View`
+  align-items: center;
+  gap: 2px;
+`
+
+export const Title = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+  font-weight: 500;
+`
+
+export const Subtitle = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_300};
+`
+
+export const EnvironmentCard = styled.View`
+  background-color: #f8f8f8;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.COLORS.GRAY_100};
+  padding: 16px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const EnvironmentCardTitle = styled.Text`
+  font-size: ${({ theme }) => theme.FONT_SIZE.LG}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+  font-weight: 500;
+`
