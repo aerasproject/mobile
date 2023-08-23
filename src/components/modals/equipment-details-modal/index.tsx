1b233041b57b842b24e72256eae48b2591ea27ca
rbpolim <rbpolim@gmail.com>
2023-08-23 11:10:51 -0300

    feat: new equipment-details-modal component w/ styles

--- src/components/modals/equipment-details-modal/index.tsx
+++ src/components/modals/equipment-details-modal/index.tsx
@@ -1,14 +1,17 @@
-import { useAddress } from '@/hooks/use-address'
+import { Link } from 'expo-router'
 
 import { EquipmentDTO } from '@/dtos'
 
-import { Modal, ModalRef } from '@/components/modal'
+import { useAddress } from '@/hooks/use-address'
+
+import { Button } from '@/components/button'
+import { Modal, ModalRefProps } from '@/components/modal'
 
 import * as S from './styles'
 
 type EquipmentDetailsModalProps = {
   equipment: EquipmentDTO
-  modalRef: React.RefObject<ModalRef>
+  modalRef: React.RefObject<ModalRefProps>
 }
 
 export function EquipmentDetailsModal({
@@ -17,21 +20,62 @@ export function EquipmentDetailsModal({
 }: EquipmentDetailsModalProps) {
   const { mainAddress } = useAddress()
 
+  console.log('equipment', equipment)
+
   return (
     <Modal ref={modalRef} height="75%">
       <S.Container>
-        {/* ADDRESS SECTION */}
-        <S.Title>ADDRESS</S.Title>
-        <S.Text>{mainAddress.street}</S.Text>
-        <S.Text>{mainAddress.street}</S.Text>
+        <S.Header>
+          <S.TitleHeader>{mainAddress.name}</S.TitleHeader>
+        </S.Header>
 
-        {/* EQUIPMENT SECTION */}
-        <S.Title>EQUIPMENT</S.Title>
-        <S.Text>{equipment.name}</S.Text>
-        <S.Text>{equipment.model}</S.Text>
-        <S.Text>{equipment.capacity}</S.Text>
-        <S.Text>{equipment.type}</S.Text>
-        <S.Text>{equipment.voltage}</S.Text>
+        <S.Content>
+          <S.Title>{equipment.name}</S.Title>
+          <S.Badge>{equipment.tag}</S.Badge>
+
+          {/* TODO: Add um divider style */}
+          {/* DIVIDER */}
+
+          <S.Wrapper>
+            <S.Box>
+              <S.Label>Rua</S.Label>
+              <S.Text>{equipment.brand}</S.Text>
+            </S.Box>
+            <S.Box>
+              <S.Label>Rua</S.Label>
+              <S.Text>{equipment.model}</S.Text>
+            </S.Box>
+            <S.Box>
+              <S.Label>Rua</S.Label>
+              <S.Text>{equipment.voltage}</S.Text>
+            </S.Box>
+            <S.Box>
+              <S.Label>Rua</S.Label>
+              <S.Text>{equipment.type}</S.Text>
+            </S.Box>
+            <S.Box>
+              <S.Label>Rua</S.Label>
+              <S.Text>{equipment.capacity}</S.Text>
+            </S.Box>
+            <S.Box>
+              <S.Label>Rua</S.Label>
+              <S.Text>{equipment.environment?.name}</S.Text>
+            </S.Box>
+          </S.Wrapper>
+        </S.Content>
+
+        <S.BtnWrapper>
+          <Button variants="danger-outline" title="Excluir" />
+          <Link
+            asChild
+            href={{
+              pathname: '/(client)/dashboard/equipment',
+              // params: { addressId: address.id },
+            }}
+          >
+            <Button title="Editar" />
+          </Link>
+        </S.BtnWrapper>
       </S.Container>
     </Modal>
   )
