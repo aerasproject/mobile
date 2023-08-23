fd958f215642d9da2b5ae2e3b6eb4fed15021726
rbpolim <rbpolim@gmail.com>
2023-08-22 21:05:47 -0300

    feat: new equipment-details-modal component w/ styles

---
+++ src/components/modals/equipment-details-modal/index.tsx
@@ -0,0 +1,38 @@
+import { useAddress } from '@/hooks/use-address'
+
+import { EquipmentDTO } from '@/dtos'
+
+import { Modal, ModalRef } from '@/components/modal'
+
+import * as S from './styles'
+
+type EquipmentDetailsModalProps = {
+  equipment: EquipmentDTO
+  modalRef: React.RefObject<ModalRef>
+}
+
+export function EquipmentDetailsModal({
+  equipment,
+  modalRef,
+}: EquipmentDetailsModalProps) {
+  const { mainAddress } = useAddress()
+
+  return (
+    <Modal ref={modalRef} height="75%">
+      <S.Container>
+        {/* ADDRESS SECTION */}
+        <S.Title>ADDRESS</S.Title>
+        <S.Text>{mainAddress.street}</S.Text>
+        <S.Text>{mainAddress.street}</S.Text>
+
+        {/* EQUIPMENT SECTION */}
+        <S.Title>EQUIPMENT</S.Title>
+        <S.Text>{equipment.name}</S.Text>
+        <S.Text>{equipment.model}</S.Text>
+        <S.Text>{equipment.capacity}</S.Text>
+        <S.Text>{equipment.type}</S.Text>
+        <S.Text>{equipment.voltage}</S.Text>
+      </S.Container>
+    </Modal>
+  )
+}
