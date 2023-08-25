ae3bf8b202c13b0cdf50064da2742a2e9919a35c
rbpolim <rbpolim@gmail.com>
2023-08-25 19:46:38 -0300

    feat: installed react-query lib

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Slot } from 'expo-router'
 import { ThemeProvider } from 'styled-components/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { Loading } from '@/components/loading'
 
@@ -13,18 +14,22 @@ import theme from '@/theme'
 
 // SplashScreen.preventAutoHideAsync()
 
+const queryClient = new QueryClient()
+
 export default function RootLayout() {
   const { isLoadingUserStorage } = useAuth()
 
   return (
     <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <AuthContextProvider>
-          <AddressContextProvider>
-            {isLoadingUserStorage ? <Loading /> : <Slot />}
-          </AddressContextProvider>
-        </AuthContextProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <AuthContextProvider>
+            <AddressContextProvider>
+              {isLoadingUserStorage ? <Loading /> : <Slot />}
+            </AddressContextProvider>
+          </AuthContextProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
     </SafeAreaProvider>
   )
 }
