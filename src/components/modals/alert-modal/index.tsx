adb1e9cb7dee57fc21111a6318b2bb8cf1bfb212
rbpolim <rbpolim@gmail.com>
2023-08-25 19:55:51 -0300

    feat: improved alert-modal & styled

---
+++ src/components/modals/alert-modal/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import Modal from 'react-native-modal'
+
+import { Button } from '@/components/button'
+
+import * as S from './styles'
+
+type AlertModalProps = {
+  onConfirm: () => void
+  description: string
+  isModalVisible: boolean
+  setModalVisible: (value: boolean) => void
+}
+
+export function AlertModal({
+  onConfirm,
+  isModalVisible,
+  setModalVisible,
+  description,
+}: AlertModalProps) {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+
+  return (
+    <Modal
+      backdropColor="black"
+      backdropOpacity={0.9}
+      animationIn="fadeIn"
+      isVisible={isModalVisible}
+    >
+      <S.Content>
+        <S.Header>
+          <Ionicons name="warning" size={32} color="#FFB84E" />
+          <S.HeaderTitle>ATENÇÃO</S.HeaderTitle>
+        </S.Header>
+
+        <S.Description>{description}</S.Description>
+
+        <S.ButtonsWrapper>
+          <Button
+            title="Cancel"
+            variants="danger-outline"
+            onPress={() => setModalVisible(false)}
+          />
+          <Button title="Excluir" onPress={onConfirm} />
+        </S.ButtonsWrapper>
+      </S.Content>
+    </Modal>
+  )
+}
---
+++ src/components/modals/alert-modal/styles.ts
@@ -0,0 +1,42 @@
+import { Dimensions } from 'react-native'
+import styled from 'styled-components/native'
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window')
+
+export const Content = styled.View`
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  padding: 16px;
+  gap: 32px;
+  border-radius: 16px;
+  width: ${SCREEN_WIDTH * 0.9}px;
+`
+
+export const Header = styled.View`
+  background-color: #fff1dc;
+  border-radius: 16px;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  gap: 8px;
+`
+
+export const HeaderTitle = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #ffb84e;
+`
+
+export const Description = styled.Text`
+  font-size: 14px;
+  text-align: center;
+  padding: 0 16px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.COLORS.GRAY_500};
+`
+
+export const ButtonsWrapper = styled.View`
+  flex-direction: row;
+
+  gap: 8px;
+  justify-content: space-between;
+`
