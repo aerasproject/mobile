22c86fd14501d406f845703b35fdccd7c82bfe08
rbpolim <rbpolim@gmail.com>
2023-08-25 19:54:28 -0300

    fix: fixed and improved address-details-modal component

--- src/components/modals/address-details-modal/index.tsx
+++ src/components/modals/address-details-modal/index.tsx
@@ -1,23 +1,61 @@
-import { Link } from 'expo-router'
+import { Alert } from 'react-native'
+import { useState } from 'react'
+
+import { api } from '@/lib/axios'
+
+import { AppError } from '@/utils/app-error'
+
+import { Link, useRouter } from 'expo-router'
 
 import { AddressDTO } from '@/dtos'
 
 import { Button } from '@/components/button'
-import { Modal, ModalRef } from '@/components/modal'
+import { AlertModal } from '@/components/modals/alert-modal'
+import { ModalHalfScreen, ModalRefProps } from '@/components/modal-half-screen'
 
 import * as S from './styles'
 
 type AddressDetailsModalProps = {
   address: AddressDTO
-  modalRef: React.RefObject<ModalRef>
+  modalRef: React.RefObject<ModalRefProps>
 }
 
 export function AddressDetailsModal({
   address,
   modalRef,
 }: AddressDetailsModalProps) {
+  const router = useRouter()
+
+  const [isModalVisible, setModalVisible] = useState(false)
+
+  async function onConfirm() {
+    try {
+      await api.delete(`/client/address/${address.id}`)
+
+      setModalVisible(false)
+
+      modalRef.current?.toggle()
+
+      router.push('/(client)/dashboard/addresses/')
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      if (isAppError) {
+        Alert.alert(error.message)
+      }
+    }
+  }
+
+  const description = `Você deseja mesmo excluir o endereço "${address.name}"??`
+
   return (
-    <Modal ref={modalRef} height="75%">
+    <ModalHalfScreen ref={modalRef} height="75%">
+      <AlertModal
+        description={description}
+        isModalVisible={isModalVisible}
+        setModalVisible={setModalVisible}
+        onConfirm={onConfirm}
+      />
+
       <S.Container>
         <S.Header>
           <S.TitleHeader>RBC</S.TitleHeader>
@@ -66,18 +104,22 @@ export function AddressDetailsModal({
         </S.Content>
 
         <S.BtnWrapper>
-          <Button variants="primary-outline" title="Excluir" />
+          <Button
+            title="Excluir"
+            variants="danger-outline"
+            onPress={() => setModalVisible(true)}
+          />
           <Link
             asChild
             href={{
-              pathname: '/(client)/dashboard/address',
               params: { addressId: address.id },
+              pathname: '/(client)/dashboard/address',
             }}
           >
             <Button title="Editar" />
           </Link>
         </S.BtnWrapper>
       </S.Container>
-    </Modal>
+    </ModalHalfScreen>
   )
 }
