ac78bb1945665542b3f6648b16e87119d4abce90
rbpolim <rbpolim@gmail.com>
2023-08-28 11:30:15 -0300

    feat: improved equipments hook (use-update)

--- src/hooks/equipments/use-update.ts
+++ src/hooks/equipments/use-update.ts
@@ -1,6 +1,9 @@
-import { useMutation } from '@tanstack/react-query'
+import { Alert } from 'react-native'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { api } from '@/lib/axios'
+
+import { AppError } from '@/utils/app-error'
 
 type DataProps = {
   brand: string
@@ -14,15 +17,31 @@ type DataProps = {
 }
 
 export function useUpdateEquipment() {
-  return useMutation(['UPDATE_EQUIPMENT'], async (data: DataProps) => {
-    await api.put(`/equipment/${data.equipmentId}`, {
-      brand: data.brand,
-      capacity: data.capacity,
-      name: data.name,
-      type: data.type,
-      voltage: data.voltage,
-      addressId: data.addressId,
-      environmentId: data.environmentId,
-    })
-  })
+  const queryClient = useQueryClient()
+
+  return useMutation(
+    ['UPDATE_EQUIPMENT'],
+    async (data: DataProps) => {
+      await api.put(`/equipment/${data.equipmentId}`, {
+        brand: data.brand,
+        capacity: data.capacity,
+        name: data.name,
+        type: data.type,
+        voltage: data.voltage,
+        addressId: data.addressId,
+        environmentId: data.environmentId,
+      })
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['GET_ALL_EQUIPMENTS'])
+      },
+      onError: (error) => {
+        const isAppError = error instanceof AppError
+        if (isAppError) {
+          Alert.alert(error.message)
+        }
+      },
+    },
+  )
 }
