6a820c011587214833cb0c33fe5a68ff48f1338f
rbpolim <rbpolim@gmail.com>
2023-08-28 11:27:58 -0300

    chore: improved EmptyBox component

--- src/components/empty-box/index.tsx
+++ src/components/empty-box/index.tsx
@@ -5,19 +5,21 @@ import { Button } from '@/components/button'
 import * as S from './styles'
 
 type EmptyBoxProps = {
-  icon: React.ReactElement
+  icon?: React.ReactElement
   title: string
-  href: Href<string>
+  href?: Href<string>
 }
 
 export function EmptyBox({ icon, title, href }: EmptyBoxProps) {
   return (
     <S.Container>
-      {icon}
+      {!!icon && icon}
       <S.Title>{title}</S.Title>
-      <Link href={href} asChild>
-        <Button title="Cadastrar novo" />
-      </Link>
+      {!!href && (
+        <Link href={href} asChild>
+          <Button title="Cadastrar novo" />
+        </Link>
+      )}
     </S.Container>
   )
 }
