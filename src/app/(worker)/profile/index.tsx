594611af6fd9e2af747d6d2dfacfac11e55887fb
rbpolim <rbpolim@gmail.com>
2023-08-30 15:09:46 -0300

    chore: improved profile screen w/ styles (worker)

--- src/app/(worker)/profile/index.tsx
+++ src/app/(worker)/profile/index.tsx
@@ -1,9 +1,27 @@
+import { useAuth } from '@/hooks/use-auth'
+
+import { Badge } from '@/components/badge'
+import { Button } from '@/components/button'
+import { AvatarCircle } from '@/components/avatar-circle'
+import { ProfileDetails } from '@/components/profile-details'
+
 import * as S from './styles'
 
 export default function ProfileScreen() {
+  const { user, signOut } = useAuth()
+
   return (
     <S.Container>
-      <S.Title>Profile Screen</S.Title>
+      <S.Header>
+        <Badge title="Manutenção em dia" />
+        <AvatarCircle size="lg" title="RBC" />
+        <S.Username>{user.name}</S.Username>
+        <S.UserType>Empresa</S.UserType>
+      </S.Header>
+      <S.Content>
+        <ProfileDetails user={user} />
+        <Button variants="danger-ghost" title="Sair" onPress={signOut} />
+      </S.Content>
     </S.Container>
   )
 }
--- src/app/(worker)/profile/styles.ts
+++ src/app/(worker)/profile/styles.ts
@@ -1,5 +1,33 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View``
+export const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    alignItems: 'stretch',
+  },
+})`
+  flex: 1;
+`
 
-export const Title = styled.Text``
+export const Header = styled.View`
+  align-items: center;
+  padding: 16px;
+  gap: 8px;
+  background-color: ${({ theme }) => theme.COLORS.BRAND};
+`
+
+export const Username = styled.Text`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XXXL}px;
+  font-weight: 500;
+  text-transform: capitalize;
+`
+
+export const UserType = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GREEN_LIGHT};
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-weight: 500;
+`
+
+export const Content = styled.View`
+  padding: 0 16px;
+`
