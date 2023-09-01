7360a687f53492f0a808f001efc9b6203552cbe8
rbpolim <rbpolim@gmail.com>
2023-09-01 20:02:30 -0300

    feat: new notifications screen w/ basic styles

---
+++ src/app/(client)/dashboard/notifications/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export default function NotificationsScreen() {
+  return (
+    <S.Container>
+      <S.Title>Notifications Screen</S.Title>
+    </S.Container>
+  )
+}
