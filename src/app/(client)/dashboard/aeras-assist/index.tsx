b6ef002533263d2848bcab37e584e0e0525a30d6
rbpolim <rbpolim@gmail.com>
2023-09-01 20:02:46 -0300

    feat: new aeras-assist screen w/ basic styles

---
+++ src/app/(client)/dashboard/aeras-assist/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export default function AerasAssistScreen() {
+  return (
+    <S.Container>
+      <S.Title>Aeras Assist Screen</S.Title>
+    </S.Container>
+  )
+}
