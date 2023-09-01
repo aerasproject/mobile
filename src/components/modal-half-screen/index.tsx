53a3b7fc61f5d7c9795f888f1b7bb3c8cedf853a
rbpolim <rbpolim@gmail.com>
2023-09-01 20:03:13 -0300

    chore: improved modal-half-screen component

--- src/components/modal-half-screen/index.tsx
+++ src/components/modal-half-screen/index.tsx
@@ -1,18 +1,18 @@
 import React, { forwardRef, useImperativeHandle, useState } from 'react'
-import { Modal as ReactNativeModal } from 'react-native'
+import { Modal, ModalProps } from 'react-native'
 
 import * as S from './styles'
 
-type ModalProps = {
+type ModalHalfScreenProps = {
   height?: string
   children: React.ReactNode
-}
+} & ModalProps
 
 export type ModalRefProps = {
   toggle: () => void
 }
 
-export const ModalHalfScreen = forwardRef<ModalRefProps, ModalProps>(
+export const ModalHalfScreen = forwardRef<ModalRefProps, ModalHalfScreenProps>(
   ({ height = '50%', children }, ref) => {
     const [isModalOpen, setIsModalOpen] = useState(false)
 
@@ -21,7 +21,7 @@ export const ModalHalfScreen = forwardRef<ModalRefProps, ModalProps>(
     }))
 
     return (
-      <ReactNativeModal
+      <Modal
         transparent
         animationType="slide"
         visible={isModalOpen}
@@ -31,11 +31,10 @@ export const ModalHalfScreen = forwardRef<ModalRefProps, ModalProps>(
         <S.Overlay activeOpacity={0.2} onPress={() => setIsModalOpen(false)}>
           <S.Content height={height} activeOpacity={1}>
             <S.ActionIcon />
-
             {children}
           </S.Content>
         </S.Overlay>
-      </ReactNativeModal>
+      </Modal>
     )
   },
 )
