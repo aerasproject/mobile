89feb5c5605f2ac74d6e971e900f5f2518f32ef2
rbpolim <rbpolim@gmail.com>
2023-09-28 09:17:58 -0300

    feat: new hash to main-address-storage

--- src/storage/storage-config.ts
+++ src/storage/storage-config.ts
@@ -1,4 +1,5 @@
 const USER_STORAGE = '@aeras:user'
 const AUTH_TOKEN_STORAGE = '@aeras:token'
+const MAIN_ADDRESS_STORAGE = '@aeras:mainAddress'
 
-export { USER_STORAGE, AUTH_TOKEN_STORAGE }
+export { USER_STORAGE, AUTH_TOKEN_STORAGE, MAIN_ADDRESS_STORAGE }
