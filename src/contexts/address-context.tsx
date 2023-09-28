32896093f3569e4f45b2827cc96524e372c17870
rbpolim <rbpolim@gmail.com>
2023-09-28 14:59:08 -0300

    feat: created load main address fn into address context

--- src/contexts/address-context.tsx
+++ src/contexts/address-context.tsx
@@ -1,4 +1,12 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
+
+import {
+  storageMainAddressLoad,
+  storageMainAddressRemove,
+  storageMainAddressSave,
+} from '@/storage/storage-main-address'
+
+import { useGetAllAddresses } from '@/hooks/addresses/use-get-all-addresses'
 
 import { AddressDTO } from '@/dtos'
 
@@ -16,7 +24,43 @@ export const AddressContext = createContext({} as AddressContextDataProps)
 export function AddressContextProvider({
   children,
 }: AddressContextProviderProps) {
+  const { data: addresses } = useGetAllAddresses()
+
   const [mainAddress, setMainAddress] = useState<AddressDTO>({} as AddressDTO)
+
+  async function loadMainAddress() {
+    const storageMainAddress = await storageMainAddressLoad()
+
+    const hasAddresses = addresses && addresses?.length > 0
+
+    if (storageMainAddress && !hasAddresses) {
+      setMainAddress({} as AddressDTO)
+      storageMainAddressRemove()
+    }
+
+    if (storageMainAddress && hasAddresses) {
+      const mainAddress = addresses?.find(
+        (address) => address.id === storageMainAddress.id,
+      )
+
+      if (mainAddress) {
+        setMainAddress(mainAddress)
+      } else {
+        await storageMainAddressRemove()
+        setMainAddress(addresses[0])
+        await storageMainAddressSave(addresses[0])
+      }
+    }
+
+    if (!storageMainAddress && hasAddresses) {
+      setMainAddress(addresses[0])
+      await storageMainAddressSave(addresses[0])
+    }
+  }
+
+  useEffect(() => {
+    loadMainAddress()
+  }, [mainAddress])
 
   return (
     <AddressContext.Provider value={{ mainAddress, setMainAddress }}>
